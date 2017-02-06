8a73de3d930c747ccaa6be7ed5384ebd85a75a31
zhenxiaogou <532265925@qq.com>
2017-02-06 18:58:52 +0800

    finish all db.py,test tomorrow

--- www/transwarp/db.py
+++ www/transwarp/db.py
@@ -40,6 +40,9 @@
       transaction函数封装了如下功能:
         1.事务也可以嵌套,内层事务会自动合并到外层事务中,这种事务能满足99%的需求
 """
+import functools
+import threading
+import logging
 
 #global engine object:
 engine = None
@@ -74,6 +77,11 @@ class _Engine(object):
 
 
 class _DBCtx(threading.local):
+	"""
+	db模块核心对象,数据库连接的上下文对象,负责从数据库获取和释放连接
+	取得的连接是惰性连接对象,因此只有调用cursor对象时,才会真正获取数据库连接
+	该对象是一个Threadlocal对象,因此绑定在此对象上的数据仅对本线程可见
+	"""
 	def _init_(self):
 		self.connection = None
 		self.transactions = 0
@@ -94,15 +102,48 @@ class _DBCtx(threading.local):
 
 _db_ctx = _DbCtx()
 
+class _LasyConnection(object):
+		"""
+		惰性连接,获取游标时才连接数据库
+		"""
+		def _init_(self):
+			self.connection = None
+
+		def cursor(self):
+				if self.connection is None:
+					_connection = engine.connect()
+					self.connection = _connection
+				return self.connection.cursor()
+
+		def commit(self):
+			self.connection.commit()
+
+		def rollback():
+			self.connection.rollback()
+
+		def cleanup():
+			if self.connection:
+				_connection = self.connection
+				self.connection = None
+				_connection.close()
+
+
 class _ConnectionCtx(object):
 	def _enter_(self):
+		"""
+		获取惰性连接对象
+		"""
 		global _db_ctx
 		self.should_cleanup = False
 		if not _db_ctx.is_init():
 			_db_ctx.init()
 			self.should_cleanup = True
 		return self
+	
 	def _exit_(self,exctype,excvalue,traceback):
+		"""
+		释放连接
+		"""
 		global _db_ctx
 		if self.should_cleanup:
 			_db_ctx.cleanup()
@@ -110,20 +151,222 @@ class _ConnectionCtx(object):
 def connection():
 	return _ConnectionCtx()
 
-@with_connection
-def select(sql,*args):
-	pass
+def with_connection(func):
+	"""
+	装饰器.装饰with语法
+	"""
+	@functools.wraps(func)
+	def _wrapper(*args,**kw):
+		with _ConnectionCtx():
+			return func(*args,**kw):
+	return _wrapper
+
+def transaction():
+	return _TransactionCtx()
+
+def with_transaction(func):
+	@functools.wraps(func)
+	def _wrapper(*args,**kw):
+		with _TransactionCtx():
+			func(*args,**kw)
+	return _wrapper
 
 @with_connection
-def update(sql,*args):
-	pass
+def _select(sql,first,*args):
+	global _db_ctx
+	cursor = None
+	sql = sql.replace('?','%s')
+	logging.info('SQL: %s, ARGS: %s' % (sql, args))
+    try:
+        cursor = _db_ctx.connection.cursor()
+        cursor.execute(sql, args)
+        if cursor.description:
+            names = [x[0] for x in cursor.description]
+        if first:
+            values = cursor.fetchone()
+            if not values:
+                return None
+            return Dict(names, values)
+        return [Dict(names, x) for x in cursor.fetchall()]
+    finally:
+        if cursor:
+            cursor.close()
 
-@with_transaction
-def do_in_transaction():
-	pass
+
+def select_one(sql, *args):
+    """
+    执行SQL 仅返回一个结果
+    如果没有结果 返回None
+    如果有1个结果，返回一个结果
+    如果有多个结果，返回第一个结果
+    >>> u1 = dict(id=100, name='Alice', email='alice@test.org', passwd='ABC-12345', last_modified=time.time())
+    >>> u2 = dict(id=101, name='Sarah', email='sarah@test.org', passwd='ABC-12345', last_modified=time.time())
+    >>> insert('user', **u1)
+    1
+    >>> insert('user', **u2)
+    1
+    >>> u = select_one('select * from user where id=?', 100)
+    >>> u.name
+    u'Alice'
+    >>> select_one('select * from user where email=?', 'abc@email.com')
+    >>> u2 = select_one('select * from user where passwd=? order by email', 'ABC-12345')
+    >>> u2.name
+    u'Alice'
+    """
+    return _select(sql, True, *args)
+
+
+def select_int(sql, *args):
+    """
+    执行一个sql 返回一个数值，
+    注意仅一个数值，如果返回多个数值将触发异常
+    >>> u1 = dict(id=96900, name='Ada', email='ada@test.org', passwd='A-12345', last_modified=time.time())
+    >>> u2 = dict(id=96901, name='Adam', email='adam@test.org', passwd='A-12345', last_modified=time.time())
+    >>> insert('user', **u1)
+    1
+    >>> insert('user', **u2)
+    1
+    >>> select_int('select count(*) from user')
+    5
+    >>> select_int('select count(*) from user where email=?', 'ada@test.org')
+    1
+    >>> select_int('select count(*) from user where email=?', 'notexist@test.org')
+    0
+    >>> select_int('select id from user where email=?', 'ada@test.org')
+    96900
+    >>> select_int('select id, name from user where email=?', 'ada@test.org')
+    Traceback (most recent call last):
+        ...
+    MultiColumnsError: Expect only one column.
+    """
+    d = _select(sql, True, *args)
+    if len(d) != 1:
+        raise MultiColumnsError('Expect only one column.')
+    return d.values()[0]
+
+
+def select(sql, *args):
+    """
+    执行sql 以列表形式返回结果
+    >>> u1 = dict(id=200, name='Wall.E', email='wall.e@test.org', passwd='back-to-earth', last_modified=time.time())
+    >>> u2 = dict(id=201, name='Eva', email='eva@test.org', passwd='back-to-earth', last_modified=time.time())
+    >>> insert('user', **u1)
+    1
+    >>> insert('user', **u2)
+    1
+    >>> L = select('select * from user where id=?', 900900900)
+    >>> L
+    []
+    >>> L = select('select * from user where id=?', 200)
+    >>> L[0].email
+    u'wall.e@test.org'
+    >>> L = select('select * from user where passwd=? order by id desc', 'back-to-earth')
+    >>> L[0].name
+    u'Eva'
+    >>> L[1].name
+    u'Wall.E'
+    """
+    return _select(sql, False, *args)
+
+
+@with_connection
+def _update(sql, *args):
+    """
+    执行update 语句，返回update的行数
+    """
+    global _db_ctx
+    cursor = None
+    sql = sql.replace('?', '%s')
+    logging.info('SQL: %s, ARGS: %s' % (sql, args))
+    try:
+        cursor = _db_ctx.connection.cursor()
+        cursor.execute(sql, args)
+        r = cursor.rowcount
+        if _db_ctx.transactions == 0:
+            # no transaction enviroment:
+            logging.info('auto commit')
+            _db_ctx.connection.commit()
+        return r
+    finally:
+        if cursor:
+            cursor.close()
+
+
+def update(sql, *args):
+    """
+    执行update 语句，返回update的行数
+    >>> u1 = dict(id=1000, name='Michael', email='michael@test.org', passwd='123456', last_modified=time.time())
+    >>> insert('user', **u1)
+    1
+    >>> u2 = select_one('select * from user where id=?', 1000)
+    >>> u2.email
+    u'michael@test.org'
+    >>> u2.passwd
+    u'123456'
+    >>> update('update user set email=?, passwd=? where id=?', 'michael@example.org', '654321', 1000)
+    1
+    >>> u3 = select_one('select * from user where id=?', 1000)
+    >>> u3.email
+    u'michael@example.org'
+    >>> u3.passwd
+    u'654321'
+    >>> update('update user set passwd=? where id=?', '***', '123')
+    0
+    """
+    return _update(sql, *args)
+
+
+def insert(table, **kw):
+    """
+    执行insert语句
+    >>> u1 = dict(id=2000, name='Bob', email='bob@test.org', passwd='bobobob', last_modified=time.time())
+    >>> insert('user', **u1)
+    1
+    >>> u2 = select_one('select * from user where id=?', 2000)
+    >>> u2.name
+    u'Bob'
+    >>> insert('user', **u2)
+    Traceback (most recent call last):
+      ...
+    IntegrityError: 1062 (23000): Duplicate entry '2000' for key 'PRIMARY'
+    """
+    cols, args = zip(*kw.iteritems())
+    sql = 'insert into `%s` (%s) values (%s)' % (table, ','.join(['`%s`' % col for col in cols]), ','.join(['?' for i in range(len(cols))]))
+    return _update(sql, *args)
+
+
+class Dict(dict):
+    """
+    字典对象
+    实现一个简单的可以通过属性访问的字典，比如 x.key = value
+    """
+    def __init__(self, names=(), values=(), **kw):
+        super(Dict, self).__init__(**kw)
+        for k, v in zip(names, values):
+            self[k] = v
+
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(r"'Dict' object has no attribute '%s'" % key)
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+
+class DBError(Exception):
+    pass
+
+
+class MultiColumnsError(DBError):
+    pass
 
 class _TransactionCtx(object):
 	def _enter_(self):
+		"""
+		每遇到一层事务嵌套+1
+		"""
 		global _db_ctx
 		self.should_close_conn = False
 		if not _db_ctx.is_init():
@@ -132,7 +375,10 @@ class _TransactionCtx(object):
 		_db_ctx.transaction = _db_ctx.transacitons + 1
 		return self
 
-	def _exit(self,exctype,excvalue,traceback):
+	def _exit_(self,exctype,excvalue,traceback):
+		"""
+		离开一层事务嵌套-1,到0时离开
+		"""
 		global _db_ctx
 		_db_ctx.transactions = _db_ctx.transactions - 1
 		try:
