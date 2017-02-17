2548ecebe4de187e575976c794e1d87e959d3a93
zhenxiaogou <woshicainiao1211@163.com>
2017-02-17 19:00:18 +0800

    add sth

--- www/transwarp/web.py
+++ www/transwarp/web.py
@@ -247,39 +247,236 @@ class HttpError(object):
 		return _HttpError(404)
 
 	@staticmethod
-	def unauthorized():
+	def conflict():
 		"""
-		send an unauthorized response
+		send an conflict response
 		"""
-		return _HttpError(401)
+		return _HttpError(409)
 
 	@staticmethod
-	def unauthorized():
+	def internalerror():
 		"""
-		send an unauthorized response
+		raise HttpError.internalerror()
+		traceback:
+			...
+		_HttpError:500 Internal Server Error
 		"""
-		return _HttpError(401)
+		return _HttpError(500)
 
 	@staticmethod
-	def unauthorized():
+	def redirect(location):
 		"""
-		send an unauthorized response
+		raise HttpError.redirect('http://www.zhenxiaogou.com')
+		traceback:
+			..
+		_RedirectError:301 Moved Permanently,http://www.zhenxiaogou.com
 		"""
-		return _HttpError(401)
+		return _RedirectError(301,location)
 
 	@staticmethod
-	def unauthorized():
+	def found(location):
 		"""
 		send an unauthorized response
 		"""
-		return _HttpError(401)
+		return _RedirectError(302,location)
 
 	@staticmethod
-	def unauthorized():
+	def seeother(location):
 		"""
-		send an unauthorized response
+		do Temporary redirect
 		"""
-		return _HttpError(401)
+		return _RedirectError(303,location)
 
+_RESPONSE_HEADER_DICT = dict(zip(map(lambda x: x.upper(), _RESPONSE_HEADERS), _RESPONSE_HEADERS))
+
+class Request(object):
+	def __init__(self,environ):
+		self._environ = environ
+
+	def _parse_input(self):
+		"""
+		将通过wsgi传入的参数解析成字典对象返回
+		比如：request({'REQUEST_METHOD':'POST','wsgi.input':StringIO('a=1&b=Mjkjkjkjk')})
+			这里解析的就是wsgi.input对象里面的字节流
+		"""
+			def _convert(item):
+				if isinstance(item,list):
+					return [utils.to_unicode(i,value) for i in item]
+				if item.filename:
+					return MultipartFile(item)
+				return utils.to_unicode(item.value)
+			fs = cgi.FieldStorage(fp=self._environ['wsgi.input'],environ=self._environ,keep_blank_values=True)
+			inputs = dict()
+			for key in fs:
+				inputs[key] = _convert(fs[key])
+			return inputs
+
+	def _get_raw_input(self):
+		if not hasattr(self,'_raw_input'):
+			sefl._raw_input = self._parse_input()
+		return self._raw_input
+
+	def __getitem__(self,key):
+		r = self._get_raw_input()[key]
+		if isinstance(r,list):
+			return r[0]
+		return r
+
+	def get(self,key,defalut=None):
+		"""
+		实现了字典里面的get功能
+		"""
+		r = self._get_raw_input().get(key,defalut)
+		if isinstance(r,list):
+			return r[0]
+		return r
+
+	def gets(self,key):
+		r = self._get_raw_input()[key]
+		if isinstance(r,list):
+			return r[:]
+		return [r]
+
+	def input(self,**kw):
+		copy = Dict(**kw)
+		raw = self._get_raw_input()
+		for k,v in raw.iteritems():
+			cpoy[k] = v[0] if isinstance(v,list) else v
+		return copy
+
+	def get_body(self):
+		"""
+		从http post请求中取得body里面的数据，返回一个str对象
+		"""
+		fp = self._environ['wsgi.input']
+		return fp.read()
+
+	@property
+	def remote_addr(self):
+		return self._environ.get('REMOTE_ADDR','0.0.0.0')
+
+	@property
+	def document_root(self):
+		return self._environ.get('DOCUMENT_ROOT','')
+
+	@property
+	def query_string(self):
+		return self._environ.get('QUERY_STRING','')
+
+	@property
+	def environ(self):
+		return self._environ
+
+	@property
+	def request_method(self):
+		return self._environ['REQUEST_METHOD']
+
+	@property
+	def path_info(self):
+		return urllib.unquote(self._environ.get('PATH_INFO',''))
+
+	@property
+	def host(self):
+		return self._environ.get('HTTP_HOST','')
+
+	def _get_headers(self):
+		if not hasattr(self,'_headers'):
+			hdrs = {}
+			for k,v in self._environ.iteritems():
+				if k.startswith('HTTP_'):
+					hdrs[k[5:].replace('_','-').upper()] = v.decode('uft-8')
+			self._headers = hdrs
+		return self._headers
+
+	@property
+	def headers(self):
+		return dict(**self._get_headers())
+
+	def header(self,header,defalut=None):
+		return self._get_headers().get(header.upper(),defalut)
+
+	def _get_cookies(self):
+		if not hasattr(self,'_cookies'):
+			cookies = {}
+			cookie_str = self._environ.get('HTTP_COOKIE')
+			if cookie_str:
+				for c in cookie_str.split(';'):
+					pos = c.find('=')
+					if pos > 0:
+						cookies[c[:pos].strip()] = utils.unquote(c[pos+1:])
+			self._cookies = cookies
+		return self._cookies
+
+	@property
+	def cookies(self):
+		return self._get_cookies().get(name,defalut)
+
+class Response(object):
+
+	def __init__(self):
+		self._status = '200ok'
+		self._headers = {'CONTENT-TYPE':'text/html;charset=utf-8'}
+
+	def unset_header(self,name):
+		key = name.upper()
+		if key not in _RESPONSE_HEADER_DICT:
+			key = name
+		if key in self._headers:
+			del self._headers[key]
+
+	def set_header(self,name,value):
+		key = name.upper()
+		if key not in _RESPONSE_HEADER_DICT:
+			key = name
+		self._headers[key] = utils.to_str(value)
+
+	def header(self,name):
+		key = name.upper()
+		if key not in _RESPONSE_HEADER_DICT:
+			key = name
+		return self._headers.get(key)
+
+	@property
+	def headers(self):
+		L = [(_RESPONSE_HEADER_DICT.get(k,v)),v for k,v in self._headers.iteritems()]
+		if hasattr(self,'_cookies'):
+			for v in self._cookies.itervalues():
+				L.append(('Set-Cookie',v))
+		L.append(_HEADER_X_POWERED_BY)
+		return L
+
+	@property
+	def content_type(self):
+		return self.header('CONTENT-TYPE')
+
+	@content_type.setter
+	def content_type(self,value):
+		if value:
+			self.set_header('CONTENT-TYPE',value)
+		else:
+			self.unset_header('CONTENT-TYPE')
+
+	@property
+	def content_length(self):
+		return self.header('CONTENT-LENGTH')
+
+	@content_length.setter
+	def content_length(self,value):
+		self.set_header('CONTENT-LENGTH',str(value))
+
+	def delete_cookie(self,name):
+		self.set_cookie(name,'__delete__',expires=0)
+
+	def set_cookie(self,name,value,max_age,expires=None,path='/',domain)	
+	
+	
+
+	
+	
+	
+	
+	
+	
+	
 
 		
